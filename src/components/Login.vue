cb26ae83daf9b67a7e8fd905e4dbe50f1ef4a057
YuanTom <2274004370@qq.com>
2022-02-11 23:26:17 +0800

    add files

---
+++ src/components/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="login_container">Login</div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #2b4b6b;
+}
+</style>
