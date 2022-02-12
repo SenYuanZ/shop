05b7b1a1fa283f697375f95ce720525711cd71bb
YuanTom <2274004370@qq.com>
2022-02-12 11:44:29 +0800

    完成了登入功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,8 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
-
+import { Button, FormItem, Input, Form, Message } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(Button)
+Vue.use(FormItem)
+Vue.use(Input)
+Vue.use(Form)
+Vue.prototype.$message = Message
