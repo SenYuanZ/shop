22565669f6ef205dac1f40c490aac330a6167897
YuanTom <2274004370@qq.com>
2022-02-12 20:30:34 +0800

    完成用户列表功能的开发

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import router from './router'
 import './plugins/element.js'
 // 导入全局样式表
 import './assets/css/global.css'
-
+/* 导入icon图标 */
+import './assets/fonts/iconfont.css'
 import axios from 'axios'
-Vue.prototype.$http = axios
+
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use((config) => {
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
