ba1873fefc607a8b82f25f3be850aab4c599dc37
YuanTom <2274004370@qq.com>
2022-02-17 12:06:00 +0800

    半完成了商品功能开发

--- src/main.js
+++ src/main.js
@@ -10,6 +10,12 @@ import Nav from './components/nav/Header.vue'
 import './assets/fonts/iconfont.css'
 import axios from 'axios'
 import TreeTable from 'vue-table-with-tree-grid'
+/* 导入富文本编辑器 */
+import VueQuillEditor from 'vue-quill-editor'
+/* 导入富文本编辑器样式 */
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
 
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -21,8 +27,23 @@ Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
+// 全局应用富文本编辑器，
+Vue.use(VueQuillEditor)
+
 Vue.component('tree-table', TreeTable)
 Vue.component('nav-header', Nav)
+/* 全局定义一个格式化时间的过滤器 */
+Vue.filter('dateFormat', function (originVal) {
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  const d = (dt.getDate() + '').padStart(2, '0')
+
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
